46354a3042047e42c5a1c241d08e34892e5dcec5
MohamedBe <85387389+MohamedBelhedi@users.noreply.github.com>
2022-11-15 12:34:46 +0000

    change the style lil bit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Spinner from 'react-bootstrap/Spinner';
+import * as bootstrap from 'react-bootstrap'
+// import { Card, Button } from 'react-bootstrap'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
@@ -9,7 +11,7 @@ export default function App() {
   const [answer, setAnswer] = useState("")
   const [vis, setVis] = useState(false)
   const [inputAnswer, setInputAnswer] = useState("")
-const[tries,setTries]= useState("3")
+  const [tries, setTries] = useState("3")
   const randInt = Math.floor(Math.random() * 100)
   const options = {
     methode: "GET",
@@ -51,13 +53,23 @@ const[tries,setTries]= useState("3")
 
   }
 
+  // make buttons for the answers, and let them name from the answers of the Api
+
   const correctAnswers = (e) => {
 
     if (e.key === "Enter") {
-      { inputAnswer === answer ? alert("right") | setTimeout(() => { location.reload() }, 3000) : alert("wrong try again");setTries(tries-1) }
-  {tries<2?setVis(true)| setTimeout(()=>{
-    location.reload();
-  }, 3000):tries}    
+      { inputAnswer === answer ? alert("right") | setTimeout(() => { location.reload() }, 3000) : alert("wrong try again"); setTries(tries - 1) }
+      { inputAnswer === answer ? setTries(0) + setTries("you Won") : tries }
+      {
+        tries < 2 ? setVis(true) | setTimeout(() => {
+          location.reload();
+        }, 3000) : tries
+      }
+
+      const triesstyle = document.getElementById("tries")
+
+      { tries < 2 ? setTries("you lose") : null }
+      { tries < 2 ? triesstyle.style.backgroundColor = "red" : triesstyle.style.backgroundColor = "articblue" }
     }
 
   }
@@ -74,29 +86,34 @@ const[tries,setTries]= useState("3")
 
   return (
     <div className="container">
+
       <div className="row justify-items-center center col-sm-6">
-        <h1>{!text1.category ? "pls Wait loading" : text1.category}</h1>
-        <div>Tries:{tries}</div>
-        <h1>{text1.question}</h1>
-        <h2>possible answers: {text1.correct_answer === undefined ? <Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner> : text1.correct_answer} {text1.incorrect_answers === undefined ? "" : text1.incorrect_answers + " "}</h2>
+        <bootstrap.Card>
 
-        {vis ? <h2>{answer}</h2> : null}
 
-        <h3>your answer:{inputAnswer}</h3>
-        <input onChange={(e) => {
-          setInputAnswer(e.target.value)
-          // { e.target.value.includes(answer) ? alert("richtig") : alert("falsch") }
+          <h1>{!text1.category ? "pls Wait loading" : text1.category}</h1>
+          <div id="tries" className="col p-2 bd-highlight">Tries:{tries}</div>
+          <h1>{text1.question}</h1>
+          <h2>possible answers: {text1.correct_answer === undefined ? <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner> : text1.correct_answer} {text1.incorrect_answers === undefined ? "" : text1.incorrect_answers + " "}</h2>
 
-          console.log(e.target.value)
-        }}
-          onKeyDown={correctAnswers}
-          value={inputAnswer} />
+          {vis ? <h2>{answer}</h2> : null}
 
-        <button className="btn btn-primary" onClick={answerVis}>Answers</button>
+          <h3>your answer:{inputAnswer}</h3>
+          <input onChange={(e) => {
+            setInputAnswer(e.target.value)
+            // { e.target.value.includes(answer) ? alert("richtig") : alert("falsch") }
+
+            console.log(e.target.value)
+          }}
+            onKeyDown={correctAnswers}
+            value={inputAnswer} />
+
+          <button className="btn btn-primary" onClick={answerVis}>Answers</button>
+        </bootstrap.Card>
       </div>
 
-    </div>
+    </div >
   )
 }
